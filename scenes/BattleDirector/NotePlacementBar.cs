34293ffb1a91ce064fd7a9d38181aa90ee8fcad3
LifeHckr <jarodthereal@gmail.com>
2025-02-02 23:35:16 -0800

    Initial tweaks and fixes
    Might have been an issue on my end:
    ChartManager got disconnected from NoteManager

--- scenes/BattleDirector/NotePlacementBar.cs
+++ scenes/BattleDirector/NotePlacementBar.cs
@@ -18,6 +18,7 @@ public partial class NotePlacementBar : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        notePlacementBar.MaxValue = MaxValue;
         currentBarValue = 0;
         currentCombo = 0;
         comboMult = 1;
