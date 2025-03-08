d6dae53858316f1bebe9a31250a959640dcf7581
cornerloan <connorlowe21@gmail.com>
2025-03-07 23:40:57 -0800

    placed notes now instantly do their effect
    The note being placed uses it's effect at the "okay" timing, since this is the middle option for note damage this should be good enough for the player to at least feel the effect instantly.

--- scenes/BattleDirector/scripts/NotePlacementBar.cs
+++ scenes/BattleDirector/scripts/NotePlacementBar.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using FunkEngine;
 using Godot;
 
 public partial class NotePlacementBar : Node
@@ -183,13 +184,16 @@ public partial class NotePlacementBar : Node
     }
 
     // Placing a note resets the note placement bar
-    public Note PlacedNote()
+    public Note PlacedNote(BattleDirector BD, Timing timing)
     {
         _currentBarValue -= (int)(_currentNoteInstance.CostModifier * MaxValue);
 
         UpdateNotePlacementBar(_currentBarValue);
         //fullBarParticles.Emitting = false;
-        return GetNote(Input.IsActionPressed("Secondary"));
+
+        Note placedNote = GetNote(Input.IsActionPressed("Secondary"));
+        placedNote?.OnHit(BD, timing);
+        return placedNote;
     }
 
     public bool CanPlaceNote()
