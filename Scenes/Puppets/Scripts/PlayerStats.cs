ef7acc80a572fd1763b9dd0befe572393a98507b
LifeHckr <jarodthereal@gmail.com>
2025-05-11 18:58:59 -0700

    Renovate Shop
    Reworked shop scene and script
    Made removal slightly portable, would like to eventually make it a separate menu to be reuseable.

--- Scenes/Puppets/Scripts/PlayerStats.cs
+++ Scenes/Puppets/Scripts/PlayerStats.cs
@@ -46,4 +46,9 @@ public partial class PlayerStats : Resource
     {
         CurNotes = CurNotes.Append(nSelection).ToArray();
     }
+
+    public void RemoveNote(Note nSelection)
+    {
+        CurNotes = CurNotes.Where(n => n != nSelection).ToArray();
+    }
 }
