9a6ce3631fe575419043a562b2f7edaa7c462e04
cornerloan <connorlowe21@gmail.com>
2025-06-02 18:32:59 -0700

    Note symbol selection is now checkbox

--- Scenes/UI/Options/Scripts/NoteTypeSelection.cs
+++ Scenes/UI/Options/Scripts/NoteTypeSelection.cs
@@ -1,37 +1,20 @@
 using System;
 using Godot;
 
-public partial class NoteTypeSelection : OptionButton
+public partial class NoteTypeSelection : CheckBox
 {
     private bool _isTypeArrow;
 
     public override void _Ready()
     {
         _isTypeArrow = SaveSystem.GetConfigValue(SaveSystem.ConfigSettings.TypeIsArrow).As<bool>();
-        Connect("item_selected", new Callable(this, nameof(OnTypeChanged)));
-        PresetDropdown(_isTypeArrow);
+        ButtonPressed = _isTypeArrow;
+        Toggled += OnTypeChanged;
     }
 
-    private void OnTypeChanged(int index)
+    private void OnTypeChanged(bool pressed)
     {
-        switch (index)
-        {
-            case 0:
-                SaveSystem.UpdateConfig(SaveSystem.ConfigSettings.TypeIsArrow, false);
-                InputHandler.UseArrows = false;
-                break;
-            case 1:
-                SaveSystem.UpdateConfig(SaveSystem.ConfigSettings.TypeIsArrow, true);
-                InputHandler.UseArrows = true;
-                break;
-        }
-    }
-
-    private void PresetDropdown(bool isTypeArrow)
-    {
-        if (isTypeArrow)
-            Selected = 1;
-        else
-            Selected = 0;
+        SaveSystem.UpdateConfig(SaveSystem.ConfigSettings.TypeIsArrow, pressed);
+        InputHandler.UseArrows = pressed;
     }
 }
